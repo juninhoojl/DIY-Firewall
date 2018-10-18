c18e0d130bc9eaa86c7b50186698b2f051c17d0d
juninhoojl <juninhopc@icloud.com>
2018-10-18 02:30:58 -0300

    modulo reset pronto

--- reset.rb
+++ reset.rb
@@ -12,7 +12,46 @@ require 'fileutils'
 # Zera todas as regras de firewall
 
 #Caminhos
-out='out/'
+out='out' #Diretorio resultado
+flags='flags'
+flagsPaises='./flags/paises.flag'
+flagVpn='./flags/vpn.flag'
+flagAtaques='./flags/ataques.flag'
 
-FileUtils.rm_rf(out)
+#Caminhos quantidades a criar
+qtdcod='./data/paises.cod' #Paises
+qtdatk='./data/nomes.ataques' #Ataques
+
+#Deleta arquivos de saida
+FileUtils.rm_rf Dir.glob("#{out}/*")
+
+#Deleta arquivos de flag
+FileUtils.rm_rf Dir.glob("#{flags}/*")
+
+#Conta quantidade de paises pelos codigos
+qtdpaises = `wc -l "#{qtdcod}"`.strip.split(' ')[0].to_i
+
+#Conta quantidade de ataques pelos nomes
+qtdataques = `wc -l "#{qtdatk}"`.strip.split(' ')[0].to_i
+
+#Recria arquivo com flag VPN
+File.open(flagVpn, 'w') { |file| file.write 0 }
+
+#Cria flags resetadas para paises
+File.open(flagsPaises, 'w') { |file| qtdpaises.times{ file.puts 0 } }
+
+#Cria flags resetadas para ataques
+File.open(flagAtaques, 'w') { |file| qtdataques.times{ file.puts 0 } }
+
+
+
+
+
+
+
+
+
+
+
+
 
